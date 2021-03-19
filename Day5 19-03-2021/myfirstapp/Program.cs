d4045140010dedbf51a96390ee32a887724bebd3
Ritik Sharma <ritik.sharma@taazaa.com>
2021-03-19 18:42:14 +0530

    C#

---
+++ Day5 19-03-2021/myfirstapp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace myfirstapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your name:");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"Hello {name.ToUpper()} on {date}");
+            Console.ReadKey(true);
+        }
+    }
+}
