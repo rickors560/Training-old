47270d82c4e8c964724cd5c35dc9581c7646d5f6
Ritik Sharma <ritik.sharma@taazaa.com>
2021-03-22 16:07:10 +0530

    C#

---
+++ Day8 22-03-2021/Polymorphism_demo/Polymorphism_demo/A.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism_demo
+{
+    public class A
+    {
+        public virtual void display(){
+            Console.WriteLine("This Base Class A");
+        }
+    }
+}
---
+++ Day8 22-03-2021/Polymorphism_demo/Polymorphism_demo/B.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism_demo
+{
+    public class B : A
+    {
+        public override void display()
+        {
+            Console.WriteLine("This is Child Class B");
+        }
+    }
+}
