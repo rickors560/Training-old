bd5e6d474e44f39796e5ce0a1bb09e696c5c9e68
Ritik Sharma <ritik.sharma@taazaa.com>
2021-03-23 08:51:53 +0530

    C#

---
+++ Day7 22-03-2021/Polymorphism_demo/Polymorphism_demo/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism_demo
+{
+    public class Person
+    {
+        public String Name { get; set; }
+        public int Age { get; set; }
+        public Person(String n, int a)
+        {
+            this.Name = n;
+            this.Age = a;
+        }
+        public override string ToString()
+        {
+            String s = this.Name + " is " + this.Age + " years old.";
+            Console.WriteLine(s);
+            return s;
+        }
+    }
+}
---
+++ Day7 22-03-2021/Polymorphism_demo/Polymorphism_demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Polymorphism_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Person obj = new Person("foo", 9);
+            obj.ToString();
+
+            Console.WriteLine("\n");
+
+            A obj1 = new A();
+            obj1.display();
+
+            B obj2 = new B();
+            obj2.display();
+
+            A obj3 = new B();
+            obj3.display();
+        }
+    }
+}
