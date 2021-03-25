e46ac1446fc0a7db30907791bb401517235c7499
Ritik Sharma <ritik.sharma@taazaa.com>
2021-03-25 18:51:52 +0530

    Events

---
+++ Day10 25-03-2021/ManagerAgentDelegateExample/ManagerAgentDelegateExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ManagerAgentDelegateExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager m1 = new Manager();
+            Message m1Message = new Message
+            {
+                Address = "abc",
+                Content = "this a message"
+            };
+            Manager.sendMessageDelegate md = SendTypes.SendByCar;
+            m1.RequestDelivery(m1Message, md);
+            Agent a = new Agent();
+            a.notificationChanged += new Agent.NotificationHandler(ShowNotification);
+        }
+        public static void ShowNotification(object theAgent, NotificationEventArgs e)
+        {
+            Console.WriteLine(e.status);
+        }
+    }
+}
